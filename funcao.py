6a7b60f6ea1932cab6bf3a04e7a88b0af9d042b4
nettofabriciox <agostinhonetto73@gmail.com>
2026-09-23 15:37:15 -0300

    Estudo funçoes

---
+++ funcao.py
@@ -0,0 +1,13 @@
+#def msg():
+   # print('Olá, mundo!')
+#print(msg()) 
+#msg()     
+
+def soma(a,b,c):
+    return (a-b)/c
+
+if __name__ == '__main__':
+    a = float(input('Digite o valor de a: '))
+    b = float(input('Digite o valor de b: '))
+    c = float(input('Digite o valor de c: '))
+    print(soma(a, b, c))
